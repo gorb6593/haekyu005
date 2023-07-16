c44568cf7ab2e8080ec1a38fbadb0e4d63628d47
gorb6593 <gorb6593@gmail.com>
2023-07-17 00:25:48 +0900

    aws연동 테스트

--- src/main/java/haekyu005/controller/MainController.java
+++ src/main/java/haekyu005/controller/MainController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
     @RequestMapping("/sbb")
+    /*
+    아래 함수으 리턴값을 그대로 브라우저에 표시
+    아래 함수의 리턴값을 문자열화 해서 부라우저 응답을 바디에 담는다
+     */
     @ResponseBody
     public String index(){
-        return "안녕하셈.";
+        return "안녕하셈2.";
     }
 }
