cd332829f6b44fe44cb6f0ae654aeb34e2e333bc
gorb6593 <gorb6593@gmail.com>
2023-07-16 23:02:49 +0900

    세팅

---
+++ src/main/java/haekyu005/Haekyu005Application.java
@@ -0,0 +1,13 @@
+package haekyu005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Haekyu005Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Haekyu005Application.class, args);
+	}
+
+}
---
+++ src/test/java/haekyu005/Haekyu005ApplicationTests.java
@@ -0,0 +1,13 @@
+package haekyu005;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Haekyu005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
