21d49ae5e077ca12633155f1945cef606bc3566a
gorb6593 <gorb6593@gmail.com>
2023-07-26 00:14:22 +0900

    리포지토리에 대해서3

--- src/main/java/haekyu005/AnswerRepository.java
+++ src/main/java/haekyu005/AnswerRepository.java
@@ -1,7 +1,14 @@
 package haekyu005;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
 
+    @Transactional
+    @Modifying
+    @Query(value = "truncate answer", nativeQuery = true)
+    void truncate();
 }
