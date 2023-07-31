e9c99e5bcd2dc16127ea2de26d8d4d407b5e88b4
gorb6593 <gorb6593@gmail.com>
2023-07-31 22:57:35 +0900

    프로젝트 마무리

--- src/main/java/haekyu005/answer/AnswerService.java
+++ src/main/java/haekyu005/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package haekyu005.answer;
 
+import haekyu005.DataNotFoundException;
 import haekyu005.question.Question;
 import haekyu005.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -21,5 +23,24 @@ public class AnswerService {
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Long id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
